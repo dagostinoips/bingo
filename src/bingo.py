64a21f5026694f785823607cf255ca4527acbd2c
Mariano <dagos@example.com>
2020-04-12 19:27:12 -0300

    Cambiado nombre de variable

--- src/bingo.py
+++ src/bingo.py
@@ -3,10 +3,10 @@
 # Los 1 representan celdas ocupadas en el carton.
 
 def carton():
-    carton = (
+    mi_carton = (
         (1,0,0,1,1,0,1,0,1),
         (0,1,0,1,0,1,1,1,1),
         (0,1,0,0,1,1,0,1,0)
     )
-    return carton
+    return mi_carton
 
