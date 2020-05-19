1da8045c536089ac836e588760b50ebef1dc2484
Mariano <dagos@example.com>
2020-05-19 18:57:46 -0300

    Algoritmo de bingo en PHP

---
+++ ejemplo-bingo.php
@@ -0,0 +1,94 @@
+<?php
+
+
+imprimirCarton(intentoCarton());
+
+
+
+// Algorimo adaptado de:
+// https://github.com/vicmagv/bingo-card-generator
+
+function intentoCarton() {
+    $contador = 0;
+
+    $carton = [
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0],
+      [0,0,0]
+    ];
+    $numerosCarton = 0;
+
+    while ($numerosCarton < 15) {
+      $contador++;
+      if ($contador == 50) {
+        return intentoCarton();
+      }
+      $numero = rand (1, 90);
+
+      $columna = floor ($numero / 10);
+      if ($columna == 9) { $columna = 8;}
+      $huecos = 0;
+      for ($i = 0; $i<3; $i++) {
+        if ($carton[$columna][$i] == 0) {
+          $huecos++;
+        }
+        if ($carton[$columna][$i] == $numero) {
+          $huecos = 0;
+          break;
+        }
+      }
+      if ($huecos < 2) {
+        continue;
+      }
+
+      $fila = 0;
+      for ($j=0; $j<3; $j++) {
+        $huecos = 0;
+        for ($i = 0; $i<9; $i++) {
+          if ($carton[$i][$fila] == 0) { $huecos++; }
+        }
+
+        if ($huecos < 5 || $carton[$columna][$fila] != 0) {
+          $fila++;
+        } else{
+          break;
+        }
+      }
+      if ($fila == 3) {
+        continue;
+      }
+
+      $carton[$columna][$fila] = $numero;
+      $numerosCarton++;
+      $contador = 0;
+    }
+
+    for ( $x = 0; $x < 9; $x++) {
+      $huecos = 0;
+      for ($y =0; $y < 3; $y ++) {
+        if ($carton[$x][$y] == 0) { $huecos++;}
+      }
+      if ($huecos == 3) {
+        return intentoCarton();
+      }
+    }
+
+    return $carton;
+}
+
+function imprimirCarton($carton) {
+    print "\n";
+    for ($columna = 0; $columna < 3; $columna++) {
+        for ($fila = 0; $fila < 9; $fila++) {
+            printf(" %2d ",$carton[$fila][$columna]);
+        }
+        print "\n";
+    }
+    print "\n";
+}
